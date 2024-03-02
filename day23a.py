f9d70ebd7f09ebbf17cd8e0af308cc1e28a64fb2
Wesley Darlington <wesley.darlington@gmail.com>
2024-03-02 09:16:05 -0500

    tidy up day 23 part 1

--- day23a.py
+++ day23a.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python3
 
-import copy
-import sys
-
-from collections import defaultdict
-
 example_input = """
 cpy 2 a
 tgl a
@@ -17,10 +12,9 @@ dec a
 
 def tgl(ram, pc, x):
     if pc+x < 0 or len(ram) <= pc+x:
-        print ('attempt made to toggle instruction out of program '
-              f'@ram[{pc=}]={ram[pc]}: {pc+x=} not in [0,{len(ram)=}]')
+        #print ('attempt made to toggle instruction out of program '
+        #      f'@ram[{pc=}]={ram[pc]}: {pc+x=} not in [0,{len(ram)=}]')
         return
-    #print(f'tgl {x}: ram[{pc+x=}]={ram[pc+x]} => ', end='')
     if len(ram[pc+x]) == 2:
         if ram[pc+x][0] == 'inc':
             ram[pc+x][0] = 'dec'
@@ -33,7 +27,6 @@ def tgl(ram, pc, x):
             ram[pc+x][0] = 'jnz'
     else:
         assert False, f'weird instr at {pc+x=}: {ram[pc+x]=}'
-    #print(f'{ram[pc+x]}')
 
 class CPU(object):
     def __init__(self, inp):
@@ -44,63 +37,26 @@ class CPU(object):
         if op in 'abcd':
             return self.registers[op]
         return int(op)
-    def print(self):
-        rr = 'abcd'
-        tab = 20 
-        for i, instr in enumerate(self.ram):
-            if i == self.pc:
-                line = '=> '
-            else:
-                line = '   '
-            line += ' '.join(instr)
-            print(line, end='')
-            if i < 4:
-                r = rr[i]
-                print(' '*(tab - len(line)), f'{r}={self.registers[r]}')
-            else:
-                print()
-        print()
-
-def step1(cpu):
-    cpu = copy.deepcopy(cpu)
-    match cpu.ram[cpu.pc]:
-        case ['cpy', src, dst]:
-            cpu.registers[dst] = cpu.lookup(src)
-        case ['inc', r]:
-            cpu.registers[r] += 1
-        case ['dec', r]:
-            cpu.registers[r] -= 1
-        case ['jnz', r, o]:
-            if cpu.lookup(r) != 0:
-                cpu.pc += cpu.lookup(o)-1
-        case ['tgl', x]:
-            tgl(cpu.ram, cpu.pc, cpu.lookup(x))
-        case _:
-            assert False, ram[pc]
-    cpu.pc += 1
-    return cpu
-
-
-def run(inp):
-    cpu = CPU(inp)
-    #cpu.print()
-    #n = 0
-    while 0 <= cpu.pc < len(cpu.ram):
-        #if n:
-        #    n -= 1
-        #else:
-        #    x = sys.stdin.readline()
-        #    try:
-        #        n = int(x)
-        #    except ValueError:
-        #        pass
-        cpu = step1(cpu)
-        #if not n or cpu.pc > 14:
-        #    cpu.print()
-    #cpu.print()
-    return cpu.registers['a']
-
-#assert run(example_input) == 3
+    def step(self):
+        match self.ram[self.pc]:
+            case ['cpy', src, dst]:
+                self.registers[dst] = self.lookup(src)
+            case ['inc', r]:
+                self.registers[r] += 1
+            case ['dec', r]:
+                self.registers[r] -= 1
+            case ['jnz', r, o]:
+                if self.lookup(r) != 0:
+                    self.pc += self.lookup(o)-1
+            case ['tgl', x]:
+                tgl(self.ram, self.pc, self.lookup(x))
+            case _:
+                assert False, (pc, registers, ram[self.pc])
+        self.pc += 1
+    def run(self):
+        while 0 <= self.pc < len(self.ram):
+            self.step()
+        return self.registers['a']
 
 real_input = open('inputs/day23.input.txt').read()
-print(run(real_input))  # 318020 is too high
+print(CPU(real_input).run())  # => 12860
